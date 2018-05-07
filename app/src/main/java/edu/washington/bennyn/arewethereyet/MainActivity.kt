275cf9ffb9b99904cad75f94fcd26c39a02e7579
Benny Nguyen <benny.nguyenx@gmail.com>
2018-05-07 14:00:34 -0700

    Made layout

--- app/src/main/java/edu/washington/bennyn/arewethereyet/MainActivity.kt
+++ app/src/main/java/edu/washington/bennyn/arewethereyet/MainActivity.kt
@@ -1,5 +1,8 @@
 package edu.washington.bennyn.arewethereyet
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    class CustomReceiver: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //add in function to stop listening for intents
     }
 }

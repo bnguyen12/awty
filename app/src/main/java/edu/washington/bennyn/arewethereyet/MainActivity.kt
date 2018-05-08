2e47ca80185eeb211a491cde23c3bf0fb1ba69d7
Benny Nguyen <benny.nguyenx@gmail.com>
2018-05-07 19:35:40 -0700

    Finish app implementation and add screenshot for emulator

--- app/src/main/java/edu/washington/bennyn/arewethereyet/MainActivity.kt
+++ app/src/main/java/edu/washington/bennyn/arewethereyet/MainActivity.kt
@@ -1,26 +1,64 @@
 package edu.washington.bennyn.arewethereyet
 
+import android.app.Activity
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textMessage : EditText = findViewById(R.id.textMessage)
+        val phoneNum: EditText = findViewById(R.id.phoneNum)
+        val minuteBox: EditText = findViewById(R.id.minutesBox)
+        val startStopBtn: Button = findViewById(R.id.startStopBtn)
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, CustomReceiver::class.java)
+
+        startStopBtn.setOnClickListener {
+            if (startStopBtn.text == "Start") {
+                if (!textMessage.text.isEmpty() &&
+                        phoneNum.text.length == 10 &&
+                        !minuteBox.text.isBlank() &&
+                        minuteBox.text[0] != '0') {
+                    Toast.makeText(this, "Alarm initiated!", Toast.LENGTH_SHORT).show()
+                    intent.putExtra("message", textMessage.text.toString())
+                    intent.putExtra("phoneNum", phoneNum.text.toString())
+                    val pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+                    val minutes = minuteBox.text.toString().toInt() * 60 * 1000
+                    alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), minutes.toLong(), pendingIntent)
+                    startStopBtn.text = "Stop"
+                }
+            } else { //If stop button is clicked
+                val pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+                alarmManager.cancel(pendingIntent)
+                startStopBtn.text = "Start"
+            }
+        }
     }
 
     class CustomReceiver: BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            val message = intent!!.getStringExtra("message")
+            val phoneNum = intent!!.getStringExtra("phoneNum")
+            val properNum = "(${phoneNum.substring(0, 3)}) ${phoneNum.substring(3, 6)}-${phoneNum.substring(6)}"
+            val fullMessage = "${properNum}: ${message}"
+            Log.e("onReceiveTag", fullMessage)
+            Toast.makeText(context, fullMessage, Toast.LENGTH_SHORT).show()
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        //add in function to stop listening for intents
     }
 }
